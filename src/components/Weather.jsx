a707d593a5b6141f8ab2a311e8420e97d751e3bc
MoizInayat <123601592+MoizInayat@users.noreply.github.com>
2024-08-14 17:08:21 -0600

    api key is not working, trying to fix it

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,41 +1,37 @@
-import React, { useEffect } from 'react'
-import './Weather.css'
-import search_icon from '../assets/search.png'
-import clear_icon from '../assets/clear.png'
-import humidity_icon from '../assets/humidity.png'
-import wind_icon from '../assets/wind.png'
-
-
-
-
+import React, { useEffect } from 'react';
+import './Weather.css';
+import search_icon from '../assets/search.png';
+import clear_icon from '../assets/clear.png';
+import humidity_icon from '../assets/humidity.png';
+import wind_icon from '../assets/wind.png';
 
 const Weather = () => {
-
-  const search = async(city)=>{
-    try{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
-
+  
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
-
     } catch (error) {
-
+      console.error('Error fetching weather data:', error);
     }
-  }
+  };
 
-  useEffect (()=>{
-    search("London")
-  },[])
+  useEffect(() => {
+    // Log the API key to the console
+    console.log(import.meta.env.VITE_APP_ID);
+    
+    search("London");
+  }, []);
+
   return (
     <div className='weather'>
-
       <div className='search-bar'>
         <input type="text" placeholder='Search' />
         <img src={search_icon} alt="" />
       </div>
-      
-      <img src= {clear_icon} alt='' className='weather-icon'/>
+      <img src={clear_icon} alt='' className='weather-icon'/>
       <p className='temperature'>16% c</p>
       <p className='location'>London</p>
       <div className="weather-data">
@@ -46,7 +42,6 @@ const Weather = () => {
             <span>Humidity</span>
           </div>
         </div>
-
         <div className="col">
           <img src={wind_icon} alt="" />
           <div>
@@ -54,12 +49,9 @@ const Weather = () => {
             <span>Wind Speed</span>
           </div>
         </div>
-
-       
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Weather
+export default Weather;
