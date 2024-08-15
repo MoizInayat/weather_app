26b73b16063e6cd111d4d34fa7dcb239bba26fbe
MoizInayat <123601592+MoizInayat@users.noreply.github.com>
2024-08-15 04:26:09 -0600

    add api, now api is fetching data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Weather.css';
 import search_icon from '../assets/search.png';
 import clear_icon from '../assets/clear.png';
@@ -7,24 +7,36 @@ import wind_icon from '../assets/wind.png';
 
 const Weather = () => {
   
+  const [weatherData, setWeatherData] = useState (false);
+
+  
+
   const inputRef = useRef ()
   const search = async (city) => {
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5a52644ab96635db12940c09eebb5231
+`;
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name, 
+      })
+     
+
     } catch (error) {
       console.error('Error fetching weather data:', error);
     }
   };
 
   useEffect(() => {
-    // Log the API key to the console
-    console.log(import.meta.env.VITE_APP_ID);
-    
-    search("London");
+    search("new york");
   }, []);
+
+ 
 
   return (
     <div className='weather'>
@@ -33,20 +45,20 @@ const Weather = () => {
         <img src={search_icon} alt="" onClick={()=>(inputRef.current.value)}/>
       </div>
       <img src={clear_icon} alt='' className='weather-icon'/>
-      <p className='temperature'>16 c</p>
-      <p className='location'>London</p>
+      <p className='temperature'>{weatherData.temperature}c</p>
+      <p className='location'>{weatherData.location}</p>
       <div className="weather-data">
         <div className="col">
           <img src={humidity_icon} alt="" />
           <div>
-            <p>91%</p>
+            <p>{weatherData.humidity}%</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="col">
           <img src={wind_icon} alt="" />
           <div>
-            <p>3.6 Km/h</p>
+            <p>{weatherData.windSpeed} Km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
@@ -56,3 +68,12 @@ const Weather = () => {
 }
 
 export default Weather;
+
+
+// VITE_APP_ID="5a52644ab96635db12940c09eebb5231"
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=$Karachi&appid=5a52644ab96635db12940c09eebb5231`;
+
+// https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=5a52644ab96635db12940c09eebb5231
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}
